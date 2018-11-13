2b02ceb31f00c2d5293ba9aacaa306521adc956c
Sealy <as70906n@pace.edu>
2018-11-12 22:51:22 -0500

    New Code For Soccer Simulation

---
+++ sim_primer_matches.R
@@ -0,0 +1,11 @@
+install.packages('dplyr')
+
+library(dplyr)
+source('clean_data.R')
+
+#get score of match
+
+get_score <- function(home, away){
+  #try to get from history, pair
+  subset <- his_pair.pl[which(hist_pair.pl$HomeTeam == home & hist_pair]dplyr
+}
