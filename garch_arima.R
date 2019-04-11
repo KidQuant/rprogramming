a55362550089e990ca9f8d4e68e319c1f112ed7d
Sealy <as70906n@pace.edu>
2019-04-10 22:57:25 -0400

    updates

---
+++ garch_arima.R
@@ -0,0 +1,102 @@
+install.packages(c("quantmod", "lattice", "timeSeries", "rugarch"))
+
+# Import the necessary libraries
+library(quantmod)
+library(lattice)
+library(timeSeries)
+library(rugarch)
+
+# Obtain the S&P500 returns and truncate the NA value
+
+getSymbols("^GSPC", from="1950-01-01")
+spReturns = diff(log(Cl(GSPC)))
+spReturns[as.character(head(index(Cl(GSPC)),1))] = 0
+
+# Create the forecasts vector to store the predictions
+
+windowLength = 500
+foreLength = length(spReturns) - windowLength
+forecasts <- vector(mode="character", length=foreLength)
+
+for (d in 0:foreLength){
+
+  # obtain the S&P 500 rolling window for this day
+  
+  spReturnsOffset = spReturns[(1+d):(windowLength)]
+  
+  # Fit the ARIMA model
+  final.aic <- Inf
+  final.order <- c(0,0,0)
+  for (p in 0:5) for (q in 0:5){
+    if ( p == 0 && q ==0) {
+      next
+      
+    }
+    
+    arimaFit = tryCatch( arima(spReturnsOffset, order = c(p, 0, q)),
+                         error=function( err ) FALSE,
+                         warning=function( err ) FALSE )
+    if( !is.logical( arimaFit ) ) {
+      current.aic <- AIC(arimaFit)
+      if (current.aic < final.aic) {
+        final.aic <- current.aic
+        final.order <- c(p, 0, q)
+        final.arima <-  arima(spReturnsOffset, order=final.order)
+      }
+    } else {
+      next
+    }
+  }
+  
+  # Specify and fit the GARCH model
+  spec = ugarchspec(
+    variance.model=list(garchOrder=c(1,1)),
+    mean.model=list(armaOrder=c(
+      final.order[1], final.order[3]
+    ), include.mean=T),
+    distribution.model = "sged"
+  )
+  fit = tryCatch(
+    ugarchfit(
+      spec, spReturnsOffset, solver = 'hybrid'
+    ), error=function(e) e, warning=function(w) w
+  )
+  
+  # If the GARCH model does not converge, set the direction to "long" else
+  # choose the correct forecast direction based on the returns prediction
+  # Output the results to the screen and the forecasts vector
+  if(is(fit, "warning")){
+    forecasts[d+1] = paste(
+      index(spReturnsOffset[windowLength]), 1, sep=","
+    )
+    print(
+      paste(
+        index(spReturnsOffset[windowLength]), 1, sep=","
+      )
+    )
+  } else {
+    fore = ugarchforecast(fit, n.ahead=1)
+    ind = fore@forecast$seriesFor
+    forecasts[d+1] = paste(
+      colnames(ind), ifelse(ind[1] < 0, -1, 1), sep=","
+    )
+    print(paste(colnames(ind), ifelse(ind[1] < 0, -1, 1), sep=","))
+  }
+}
+
+# Output the CSV file to "forecast.csv"
+write.csv(forecasts, file="forecast.csv", row.names = FALSE)
+
+# Input the Python-refined CSV file AFTER CONVERSION
+spArimaGarch = as.xts(
+  read.zoo(
+    file="forecast_new.csv", format="%Y-%m-%d", header=F, sep="," 
+  )
+)
+
+# Create the ARIMA+GARCH returns
+spIntersect = merge( apArimaGarch[,1], spReturns, all=F )
+spArimaGarchReturns = spIntersect[,1] * spIntersect[,2]
+
+# Create the backtest for ARIMA+GARCH and Buy & Hold
+
